29cc370373a4726f23d97963d80296e7b40ed24a
miyagi389 <miyagi389@gmail.com>
2006-03-15 13:57:06 +0000

    Implフォルダに統一
    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk@155 6374060a-8204-0410-be21-af3d0958e6ef

---
+++ s2container.net/source/Seasar.Tests/Seasar.Tests.Extension.DataSets.Impl/Impl/XlsReaderSortTest.cs
@@ -0,0 +1,62 @@
+#region Copyright
+/*
+ * Copyright 2005 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+#endregion
+
+using System;
+using System.Data;
+using System.IO;
+using System.Reflection;
+using MbUnit.Framework;
+using Seasar.Extension.DataSets.Impl;
+using Seasar.Extension.Unit;
+using Seasar.Framework.Util;
+
+namespace Seasar.Tests.Extension.DataSets.Impl
+{
+	[TestFixture]
+	public class XlsReaderSortTest : S2TestCase
+	{
+		private static readonly string PATH = "Seasar.Tests.Extension.DataSets.Impl.XlsReaderImplSortTest.xls";
+
+		private DataSet dataSet;
+
+		[SetUp]
+		public void SetUp() 
+		{
+			using (Stream stream = ResourceUtil.GetResourceAsStream(PATH, Assembly.GetExecutingAssembly()))  
+			{
+				dataSet = new XlsReader(stream).Read();
+			}
+		}
+
+		[Test]
+		public void TestCreateTable() 
+		{
+			Assert.AreEqual(4, dataSet.Tables.Count, "1");
+			DataSetInspector.OutWriteLine(dataSet);
+		}
+
+		[Test]
+		public void TestSort() 
+		{
+			Assert.AreEqual(dataSet.Tables[0].TableName, "TEST_TABLE", "1");
+			Assert.AreEqual(dataSet.Tables[1].TableName, "SECOND_TABLE", "2");
+			Assert.AreEqual(dataSet.Tables[2].TableName, "‚ ", "3");
+			Assert.AreEqual(dataSet.Tables[3].TableName, "EMPTY_TABLE", "4");
+		}
+	}
+}
---
+++ s2container.net/source/Seasar.Tests/Seasar.Tests.Extension.DataSets.Impl/Impl/XlsWriterTest.cs
@@ -0,0 +1,67 @@
+#region Copyright
+/*
+ * Copyright 2005 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+#endregion
+
+using System.Data;
+using System.IO;
+using System.Reflection;
+using MbUnit.Framework;
+using Seasar.Extension.DataSets;
+using Seasar.Extension.DataSets.Impl;
+using Seasar.Extension.Unit;
+using Seasar.Framework.Util;
+
+namespace Seasar.Tests.Extension.DataSets.Impl
+{
+	[TestFixture]
+	public class XlsWriterTest : S2TestCase
+	{
+		private static readonly string PATH = "Seasar.Tests.Extension.DataSets.Impl.XlsReaderImplTest.xls";
+
+		private string path2_;
+
+		private DataSet dataSet_;
+
+		private IDataWriter writer_;
+
+		[SetUp]
+		public void SetUp() 
+		{
+			using (Stream stream = ResourceUtil.GetResourceAsStream(PATH, Assembly.GetExecutingAssembly()))  
+			{
+				dataSet_ = new XlsReader(stream).Read();
+			}
+			path2_ = Path.Combine(Path.GetTempPath(), "XlsWriterImplTest.xls");
+			writer_ = new XlsWriter(path2_);
+		}
+
+		[TearDown]
+		public void TearDown() 
+		{
+			if (File.Exists(path2_)) 
+			{
+				File.Delete(path2_);
+			}
+		}
+
+		[Test]
+		public void TestWrite() 
+		{
+			writer_.Write(dataSet_);
+		}
+	}
+}
