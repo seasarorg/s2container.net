16dfae6e97dd40d203193b6296cb24b957f8936f
taichi <ryushi@gmail.com>
2006-04-08 19:23:13 +0000

    .NET2.0対応トランザクションアドバイス。未テスト、未実行。
    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk/s2container.net@209 6374060a-8204-0410-be21-af3d0958e6ef

---
+++ source/Seasar/Seasar.Extension.Tx/Impl/RequiredTxHandler.cs
@@ -0,0 +1,64 @@
+#region Copyright
+/*
+ * Copyright 2005 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+#endregion
+
+using System.Transactions;
+
+using Seasar.Framework.Aop;
+using Seasar.Framework.Log;
+
+namespace Seasar.Extension.Tx.Impl
+{
+    public class RequiredTxHandler : ITransactionHandler
+    {
+        private static Logger logger = Logger.GetLogger(typeof(RequiredTxHandler));
+
+        #region ITransactionHandler ÉÅÉìÉo
+
+        object ITransactionHandler.Handle(IMethodInvocation invocation, bool alreadyInTransaction)
+        {
+            bool began = !alreadyInTransaction;
+
+            if (began)
+            {
+                logger.Log("DSSR0003", null);
+            }
+
+            using (TransactionScope scope = new TransactionScope(TransactionScopeOption.Required))
+            {
+                try
+                {
+                    object obj = invocation.Proceed();
+                    if (began)
+                    {
+                        logger.Log("DSSR0004", null);
+                    }
+                    scope.Complete();
+                    return obj;
+                } catch {
+                    if (began)
+                    {
+                        logger.Log("DSSR0005", null);
+                    }
+                    throw;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
