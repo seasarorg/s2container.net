d7dc36288b68037d1a400d3ad86a25b4203f7150
sugimotokazuya <sugimotokazuya@6374060a-8204-0410-be21-af3d0958e6ef>
2006-05-24 16:14:21 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk@305 6374060a-8204-0410-be21-af3d0958e6ef

--- s2container.net/source/Seasar.Tests/Seasar.Tests.Framework.Aop/Proxy/DynamicAopProxyTest.cs
+++ s2container.net/source/Seasar.Tests/Seasar.Tests.Framework.Aop/Proxy/DynamicAopProxyTest.cs
@@ -1,4 +1,9 @@
 using System;
+using System.IO;
+using System.Reflection;
+using log4net;
+using log4net.Config;
+using log4net.Util;
 using MbUnit.Framework;
 using Seasar.Framework.Container;
 using Seasar.Framework.Container.Impl;
@@ -7,42 +12,70 @@ using Seasar.Framework.Aop;
 using Seasar.Framework.Aop.Impl;
 using Seasar.Framework.Aop.Proxy;
 using Seasar.Framework.Aop.Interceptors;
+using Seasar.Extension.Unit;
 
 namespace Seasar.Tests.Framework.Aop.Proxy
 {
     [TestFixture]
-	class DynamicAopProxyTest
+	class DynamicAopProxyTest : S2TestCase
 	{
-        [Test]
-        public void TestProxy()
+        IHello _hello = null;
+
+        public DynamicAopProxyTest()
         {
-            IS2Container container = S2ContainerFactory.Create("Seasar.Tests.Framework.Aop.Proxy.proxy.dicon");
-            container.Init();
-            IHello hello = (IHello)container.GetComponent(typeof(IHello));
-            Assert.AreEqual("Hello", hello.Greeting());
+            // log4netÇÃèâä˙âª
+            FileInfo info = new FileInfo(SystemInfo.AssemblyShortName(
+                Assembly.GetExecutingAssembly()) + ".exe.config");
+            XmlConfigurator.Configure(LogManager.GetRepository(), info);
         }
 
+        public void SetUpProxy()
+        {
+            this.Include("Seasar.Tests.Framework.Aop.Proxy.proxy.dicon");
+        }
 
+        [Test, S2]
+        public void TestProxy()
+        {
+            Assert.AreEqual("Hello", _hello.Greeting());
+        }
+
+        public void SetUpProperty()
+        {
+            this.Include("Seasar.Tests.Framework.Aop.Proxy.proxy.dicon");
+        }
+
+        [Test, S2]
+        public void TestProperty()
+        {
+            Assert.AreEqual("TestProperty", _hello.Prop);
+        }
 	}
 
     public interface IHello
     {
         string Greeting();
+        string Prop { set; get; }
     }
 
     public class HelloImpl : IHello
     {
-        private string str_ = "abc";
-
+        private string _str = "abc";
+        private string _prop = "default";
         public HelloImpl()
         {
         }
 
         public virtual string Greeting()
         {
-            return str_;
+            return _str;
         }
 
+        public string Prop
+        {
+            set { _prop = value; }
+            get { return _prop; }
+        }
     }
 
     public class HelloInterceptor : IMethodInterceptor
