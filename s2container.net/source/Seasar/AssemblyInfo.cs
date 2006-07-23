774362374b5df6d2524da0c276f84c9e2d27b88c
sugimotokazuya <sugimotokazuya@6374060a-8204-0410-be21-af3d0958e6ef>
2006-07-23 15:01:24 +0000

    S2Container.NET 1.2.2 リリース
    git-svn-id: https://www.seasar.org/svn/s2container.net/branches/s2container.net%20for%20.net%201.1@508 6374060a-8204-0410-be21-af3d0958e6ef

--- s2container.net/source/Seasar/AssemblyInfo.cs
+++ s2container.net/source/Seasar/AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Runtime.CompilerServices;
 // 情報を変更するには、これらの属性値を変更してください。
 //
 [assembly: AssemblyTitle("S2Container.NET")]
-[assembly: AssemblyDescription("S2Container.NET 1.2.1")]
+[assembly: AssemblyDescription("S2Container.NET 1.2.2")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("S2Container.NET")]
@@ -44,7 +44,7 @@ using System.Runtime.CompilerServices;
 // 下にあるように、'*' を使って、すべての値を指定するか、
 // ビルドおよびリビジョン番号を既定値にすることができます。
 
-[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.2.2.0")]
 
 //
 // アセンブリに署名するには、使用するキーを指定しなければなりません。 
