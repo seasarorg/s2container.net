83c6d66b23cdca9a81aecadc1364407d406757c3
miyagi389 <miyagi389@gmail.com>
2006-08-25 11:42:30 +0000

    S2Unitでトランザクション有効時、DBセッション情報が残る問題を修正(CONTAINERNET-37)
    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk@576 6374060a-8204-0410-be21-af3d0958e6ef

--- s2container.net/source/Seasar.Unit/Seasar.Extension.Unit/S2TestCaseRunner.cs
+++ s2container.net/source/Seasar.Unit/Seasar.Extension.Unit/S2TestCaseRunner.cs
@@ -16,98 +16,110 @@
  */
 #endregion
 
-using System;
 using System.Collections;
 using MbUnit.Core.Invokers;
 using Seasar.Extension.ADO;
 using Seasar.Extension.Tx;
+using Seasar.Extension.Tx.Impl;
 using Seasar.Framework.Container;
 using Seasar.Framework.Unit;
 using Seasar.Framework.Util;
 
 namespace Seasar.Extension.Unit
 {
-	public class S2TestCaseRunner : S2FrameworkTestCaseRunner
-	{
-		private static readonly String DATASOURCE_NAME = "Ado"
-			+ ContainerConstants.NS_SEP + "DataSource";
+    public class S2TestCaseRunner : S2FrameworkTestCaseRunner
+    {
+        private static readonly string DATASOURCE_NAME = "Ado"
+            + ContainerConstants.NS_SEP + "DataSource";
 
-		private S2TestCase fixture;
-		private Tx tx;
-		private ITransactionContext tc;
-		private IDataSource dataSource;
+        private S2TestCase fixture;
+        private Tx tx;
+        private ITransactionContext tc;
+        private IDataSource dataSource;
 
-		public S2TestCaseRunner()
-		{
-		}
+        public S2TestCaseRunner()
+        {
+        }
 
-		public object Run(IRunInvoker invoker, object o, IList args, Tx tx)
-		{
-			this.tx = tx;
-			fixture = o as S2TestCase;
-			return this.Run(invoker, o, args);
-		}
+        public object Run(IRunInvoker invoker, object o, IList args, Tx tx)
+        {
+            this.tx = tx;
+            fixture = o as S2TestCase;
+            return this.Run(invoker, o, args);
+        }
 
-		protected override void BeginTransactionContext()
-		{
-			if (Tx.NotSupported != tx)
-			{
-				tc = (ITransactionContext) this.Container.GetComponent(typeof(ITransactionContext));
-				tc.Begin();
-			}
-		}
+        protected override void BeginTransactionContext()
+        {
+            if (Tx.NotSupported != tx)
+            {
+                tc = (ITransactionContext) this.Container.GetComponent(typeof(ITransactionContext));
+                tc.Begin();
+            }
+        }
 
-		protected override void EndTransactionContext()
-		{
-			if (tc != null)
-			{
-				if (Tx.Commit == tx)
-					tc.Commit();
-				if (Tx.Rollback == tx)
-					tc.Rollback();
-			}
-		}
+        protected override void EndTransactionContext()
+        {
+            if (tc != null)
+            {
+                if (Tx.Commit == tx)
+                {
+                    tc.Commit();
+                }
+                if (Tx.Rollback == tx)
+                {
+                    tc.Rollback();
+                }
+            }
+        }
 
-		protected override void SetUpAfterContainerInit()
-		{
-			base.SetUpAfterContainerInit();
-			SetupDataSource();
-		}
+        protected override void SetUpAfterContainerInit()
+        {
+            base.SetUpAfterContainerInit();
+            SetupDataSource();
+        }
 
-		protected override void TearDownBeforeContainerDestroy()
-		{
-			TearDownDataSource();
-			base.TearDownBeforeContainerDestroy();
-		}
+        protected override void TearDownBeforeContainerDestroy()
+        {
+            TearDownDataSource();
+            base.TearDownBeforeContainerDestroy();
+        }
 
-		protected void SetupDataSource() 
-		{
-			if (this.Container.HasComponentDef(DATASOURCE_NAME)) 
-			{
-				dataSource = this.Container.GetComponent(DATASOURCE_NAME) as IDataSource;
-			} 
-			else if (this.Container.HasComponentDef(typeof(IDataSource))) 
-			{
-				dataSource = this.Container.GetComponent(typeof(IDataSource)) as IDataSource;
-			}
-			if (fixture != null && dataSource != null)
-			{
-				fixture.SetDataSource(dataSource);
-			}
-		}
+        protected void SetupDataSource()
+        {
+            if (this.Container.HasComponentDef(DATASOURCE_NAME))
+            {
+                dataSource = this.Container.GetComponent(DATASOURCE_NAME) as IDataSource;
+            }
+            else if (this.Container.HasComponentDef(typeof(IDataSource)))
+            {
+                dataSource = this.Container.GetComponent(typeof(IDataSource)) as IDataSource;
+            }
+            if (fixture != null && dataSource != null)
+            {
+                fixture.SetDataSource(dataSource);
+            }
+        }
 
-		protected void TearDownDataSource() 
-		{
-			if (fixture.HasConnection) 
-			{
-				ConnectionUtil.Close(fixture.Connection);
-				fixture.SetConnection(null);
-			}
-			if (fixture != null)
-			{
-				fixture.SetDataSource(null);
-			}
-			dataSource = null;
-		}
-	}
+        protected void TearDownDataSource()
+        {
+            if (dataSource is TxDataSource)
+            {
+                TxDataSource txDataSource = dataSource as TxDataSource;
+                if (txDataSource.Context.Connection != null)
+                {
+                    DataSourceUtil.CloseConnection(txDataSource, txDataSource.Context.Connection);
+                }
+            }
+            if (fixture.HasConnection)
+            {
+                ConnectionUtil.Close(fixture.Connection);
+                fixture.SetConnection(null);
+            }
+            if (fixture != null)
+            {
+                fixture.SetDataSource(null);
+            }
+            dataSource = null;
+        }
+    }
 }
