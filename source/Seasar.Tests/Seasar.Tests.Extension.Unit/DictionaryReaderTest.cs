19771013b85d68b8023da734d4aec391c45a9c49
sugimotokazuya <sugimotokazuya@6374060a-8204-0410-be21-af3d0958e6ef>
2006-09-04 14:36:31 +0000

    Copyright 2005 を Copyright 2005-2006 に変更する
    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk/s2container.net@588 6374060a-8204-0410-be21-af3d0958e6ef

--- source/Seasar.Tests/Seasar.Tests.Extension.Unit/DictionaryReaderTest.cs
+++ source/Seasar.Tests/Seasar.Tests.Extension.Unit/DictionaryReaderTest.cs
@@ -1,6 +1,6 @@
 #region Copyright
 /*
- * Copyright 2005 the Seasar Foundation and the Others.
+ * Copyright 2005-2006 the Seasar Foundation and the Others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
