a8fe135cb5f8335735c507e206ca330215881e50
sugimotokazuya <sugimotokazuya@6374060a-8204-0410-be21-af3d0958e6ef>
2007-01-14 14:58:20 +0000

    S2Container.NET 1.2.8 リリース
    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk@670 6374060a-8204-0410-be21-af3d0958e6ef

--- s2container.net/source/Seasar.Windows/AssemblyInfo.cs
+++ s2container.net/source/Seasar.Windows/AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Reflection;
 // 情報を変更するには、これらの属性値を変更してください。
 //
 [assembly : AssemblyTitle("S2Windows.NET")]
-[assembly: AssemblyDescription("S2Windows.NET 1.2.7")]
+[assembly: AssemblyDescription("S2Windows.NET 1.2.8")]
 [assembly : AssemblyConfiguration("")]
 [assembly : AssemblyCompany("")]
 [assembly : AssemblyProduct("S2Windows.NET")]
@@ -44,7 +44,7 @@ using System.Reflection;
 // 下にあるように、'*' を使って、すべての値を指定するか、
 // ビルドおよびリビジョン番号を既定値にすることができます。
 
-[assembly: AssemblyVersion("1.2.7.0")]
+[assembly: AssemblyVersion("1.2.8.0")]
 
 //
 // アセンブリに署名するには、使用するキーを指定しなければなりません。 
@@ -73,7 +73,7 @@ using System.Reflection;
 //
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyName("")]
-[assembly: AssemblyFileVersionAttribute("1.2.7")]
+[assembly: AssemblyFileVersionAttribute("1.2.8")]
 
 #if NET_1_1
 [assembly: AssemblyKeyFile("..\\..\\..\\Seasar.snk")]
