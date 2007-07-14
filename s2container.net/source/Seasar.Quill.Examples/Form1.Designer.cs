8de6a4429bbf9e8d134a0e51068ca912909bd916
sugimotokazuya <sugimotokazuya@6374060a-8204-0410-be21-af3d0958e6ef>
2007-07-14 07:40:44 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk@757 6374060a-8204-0410-be21-af3d0958e6ef

--- s2container.net/source/Seasar.Quill.Examples/Form1.Designer.cs
+++ s2container.net/source/Seasar.Quill.Examples/Form1.Designer.cs
@@ -29,20 +29,9 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
             this.quillControl1 = new Seasar.Quill.QuillControl();
             ((System.ComponentModel.ISupportInitialize)(this.quillControl1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(83, 38);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(73, 36);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Plusを呼ぶ";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // quillControl1
             // 
@@ -58,9 +47,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(199, 111);
+            this.ClientSize = new System.Drawing.Size(376, 263);
             this.Controls.Add(this.quillControl1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.quillControl1)).EndInit();
@@ -70,7 +58,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private QuillControl quillControl1;
 
 
