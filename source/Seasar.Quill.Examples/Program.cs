74544fe8dfb7f6d68d0c24811b0c0ebe3637ec64
sugimotokazuya <sugimotokazuya@6374060a-8204-0410-be21-af3d0958e6ef>
2007-07-19 15:04:43 +0000

    Quillのデモでトランザクションを使用するようにしました
    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk/s2container.net@761 6374060a-8204-0410-be21-af3d0958e6ef

--- source/Seasar.Quill.Examples/Program.cs
+++ source/Seasar.Quill.Examples/Program.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Windows.Forms;
 using Seasar.Framework.Container.Factory;
+using System.IO;
+using log4net.Config;
+using System.Reflection;
+using log4net;
+using log4net.Util;
 
 namespace Seasar.Quill.Examples
 {
@@ -13,6 +18,11 @@ namespace Seasar.Quill.Examples
         [STAThread]
         static void Main()
         {
+            // log4netの初期化
+            FileInfo info = new FileInfo(SystemInfo.AssemblyShortName(
+                Assembly.GetExecutingAssembly()) + ".exe.config");
+            XmlConfigurator.Configure(LogManager.GetRepository(), info);
+
             // 定義(dicon)ファイルをセットする
             SingletonS2ContainerFactory.ConfigPath = "Seasar.Quill.Examples.App.dicon";
 
