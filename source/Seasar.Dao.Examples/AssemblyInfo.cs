caf987b02df58118c27ae69ccc2d85ff2d6f60b6
sugimotokazuya <sugimotokazuya@6374060a-8204-0410-be21-af3d0958e6ef>
2007-12-09 14:13:44 +0000

    S2Container.NET 1.3.5 & S2Dao.NET 1.3.5 リリース準備
    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk/s2container.net@892 6374060a-8204-0410-be21-af3d0958e6ef

--- source/Seasar.Dao.Examples/AssemblyInfo.cs
+++ source/Seasar.Dao.Examples/AssemblyInfo.cs
@@ -43,7 +43,7 @@ using System.Reflection;
 // 下にあるように、'*' を使って、すべての値を指定するか、
 // ビルドおよびリビジョン番号を既定値にすることができます。
 
-[assembly: AssemblyVersion("1.3.4.0")]
+[assembly: AssemblyVersion("1.3.5.0")]
 
 //
 // アセンブリに署名するには、使用するキーを指定しなければなりません。 
@@ -73,4 +73,4 @@ using System.Reflection;
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
-[assembly: AssemblyFileVersionAttribute("1.3.4.0")]
+[assembly: AssemblyFileVersionAttribute("1.3.5.0")]
