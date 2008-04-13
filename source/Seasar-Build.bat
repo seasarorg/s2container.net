1680ca1f5652f76606bbb88aa4a9acf7b1193e92
miyagi389 <miyagi389@gmail.com>
2008-04-13 00:55:04 +0000

    buildフォルダにアセンブリをコピーするバッチを追加
    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk/s2container.net@971 6374060a-8204-0410-be21-af3d0958e6ef

---
+++ source/Seasar-Build.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+SETLOCAL
+
+@SET PATH=%windir%\Microsoft.NET\Framework\v3.5;%windir%\Microsoft.NET\Framework\v3.0;%windir%\Microsoft.NET\Framework\v2.0.50727;%PATH%
+
+ECHO Seasar.NETのビルドを開始します。
+rem msbuild Seasar.sln /p:Configuration=Release /t:Clean;Rebuild
+msbuild Seasar-Build.xml /target:CopyBuildFiles
+
+ENDLOCAL
