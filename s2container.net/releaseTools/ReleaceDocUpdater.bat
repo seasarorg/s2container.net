e169016a9175b5e5ee60a077a9c936fa911e5d6e
koyak <keikotani926@gmail.com>
2009-06-01 14:17:05 +0000

    1.3.16リリース準備
    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk@1130 6374060a-8204-0410-be21-af3d0958e6ef

--- s2container.net/releaseTools/ReleaceDocUpdater.bat
+++ s2container.net/releaseTools/ReleaceDocUpdater.bat
@@ -14,12 +14,12 @@ rem ----------------------------------------------------------------------------
 
 rem s2conariner.net ja
 cd C:\MyProgram\seasar\seasar_net\www_net\ja
-ReleaceDocUpdater.exe 1.3.14 1.3.15 index.html seasarnet.html download.html
+ReleaceDocUpdater.exe 1.3.15 1.3.16 index.html seasarnet.html download.html
 
 rem s2conariner.net en
 cd C:\MyProgram\seasar\seasar_net\www_net\en
-ReleaceDocUpdater.exe 1.3.14 1.3.15 index.html releases.html
+ReleaceDocUpdater.exe 1.3.15 1.3.16 index.html releases.html
 
 rem s2dao.net
 cd C:\MyProgram\seasar\seasar_net\www_daonet
-ReleaceDocUpdater.exe 1.3.14 1.3.15 ja\index.html en\index.html
+ReleaceDocUpdater.exe 1.3.15 1.3.16 ja\index.html en\index.html
