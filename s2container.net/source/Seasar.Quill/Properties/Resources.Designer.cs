2214eec0bec8662200c562638d2bf7c77d660477
koyak <keikotani926@gmail.com>
2009-06-01 16:16:07 +0000

    Quillコントロールのアイコンイメージが取得できない問題を修正
    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk@1138 6374060a-8204-0410-be21-af3d0958e6ef

--- s2container.net/source/Seasar.Quill/Properties/Resources.Designer.cs
+++ s2container.net/source/Seasar.Quill/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Seasar.Quill.Properties {
+namespace Properties {
     using System;
     
     
@@ -39,7 +39,8 @@ namespace Seasar.Quill.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Seasar.Quill.Properties.Resources", typeof(Resources).Assembly);
+                    //global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Seasar.Quill.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
