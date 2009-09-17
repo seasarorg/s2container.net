a61f4a34dd7fb901a241f168d7335cc3a6d27750
koyak <keikotani926@gmail.com>
2009-09-17 16:34:31 +0000

    1.3.17リリース準備
    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk/s2container.net@1165 6374060a-8204-0410-be21-af3d0958e6ef

--- source/Seasar.Dxo/Properties/AssemblyInfo.cs
+++ source/Seasar.Dxo/Properties/AssemblyInfo.cs
@@ -69,5 +69,5 @@ using System.Runtime.InteropServices;
 
 [assembly : CLSCompliant(true)]
 
-[assembly: AssemblyVersion("1.3.16.1")]
-[assembly: AssemblyFileVersionAttribute("1.3.16.1")]
+[assembly: AssemblyVersion("1.3.17.0")]
+[assembly: AssemblyFileVersionAttribute("1.3.17.0")]
--- source/Seasar.Quill/Properties/AssemblyInfo.cs
+++ source/Seasar.Quill/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
 [assembly: Guid("947b9d01-9c60-41c1-93fa-a4451ab163f5")]
 
-[assembly: AssemblyVersion("1.3.16.1")]
-[assembly: AssemblyFileVersionAttribute("1.3.16.1")]
+[assembly: AssemblyVersion("1.3.17.0")]
+[assembly: AssemblyFileVersionAttribute("1.3.17.0")]
