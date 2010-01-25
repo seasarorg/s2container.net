f1732eee83844c6ec5aa7daec5fe0726e879bef4
koyak <keikotani926@gmail.com>
2010-01-25 14:49:29 +0000

    [CONTAINERNET-135]ソースファイルとhtmlファイルのCopyright の"2005-2009"の文字列を"2005-2010"の文字列に更新する。
    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk/s2container.net@1185 6374060a-8204-0410-be21-af3d0958e6ef

--- source/Seasar.Windows/AssemblyInfo.cs
+++ source/Seasar.Windows/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 #region Copyright
 /*
- * Copyright 2005-2009 the Seasar Foundation and the Others.
+ * Copyright 2005-2010 the Seasar Foundation and the Others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ using System.Reflection;
 [assembly : AssemblyConfiguration("")]
 [assembly : AssemblyCompany("")]
 [assembly : AssemblyProduct("S2Windows.NET")]
-[assembly : AssemblyCopyright("c Copyright The Seasar Foundation and the others 2005-2009, all rights reserved.")]
+[assembly : AssemblyCopyright("c Copyright The Seasar Foundation and the others 2005-2010, all rights reserved.")]
 [assembly : AssemblyTrademark("")]
 [assembly : AssemblyCulture("")]
 
