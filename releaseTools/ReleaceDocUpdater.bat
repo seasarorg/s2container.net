5c783859aa66d2046cd4c82b01ac20c3a65dc04f
koyak <keikotani926@gmail.com>
2010-08-01 14:52:56 +0000

    1.3.18リリース準備
    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk/s2container.net@1207 6374060a-8204-0410-be21-af3d0958e6ef

--- releaseTools/ReleaceDocUpdater.bat
+++ releaseTools/ReleaceDocUpdater.bat
@@ -13,13 +13,13 @@ rem    3`:XV‘ÎÛ‚Æ‚È‚éƒtƒ@ƒCƒ‹ƒpƒXi•¡”w’è‚·‚éê‡‚Í‹ó”’‹æØ‚è‚ÅƒpƒX‚ğ‹Lq
 rem -----------------------------------------------------------------------------
 
 rem s2conariner.net ja
-cd C:\MyProgram\seasar\seasar_net\www_net\ja
-ReleaceDocUpdater.exe 1.3.15 1.3.16 index.html seasarnet.html download.html
+cd C:\source\seasar\www_net\ja
+ReleaceDocUpdater.exe 1.3.17 1.3.18 index.html seasarnet.html download.html
 
 rem s2conariner.net en
-cd C:\MyProgram\seasar\seasar_net\www_net\en
-ReleaceDocUpdater.exe 1.3.15 1.3.16 index.html releases.html
+cd C:\source\seasar\www_net\en
+ReleaceDocUpdater.exe 1.3.17 1.3.18 index.html releases.html
 
 rem s2dao.net
-cd C:\MyProgram\seasar\seasar_net\www_daonet
-ReleaceDocUpdater.exe 1.3.15 1.3.16 ja\index.html en\index.html
+cd C:\source\seasar\www_daonet
+ReleaceDocUpdater.exe 1.3.17 1.3.18 ja\index.html en\index.html
