8dcead72eae45f346216ad44ed39496713b8f283
koyak <keikotani926@gmail.com>
2010-12-05 01:27:47 +0000

    .NET4用のMSBuildパスを追加
    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk/s2container.net@1228 6374060a-8204-0410-be21-af3d0958e6ef

--- source/Seasar-Build.bat
+++ source/Seasar-Build.bat
@@ -1,7 +1,8 @@
 @ECHO OFF
 SETLOCAL
 
-@SET PATH=%windir%\Microsoft.NET\Framework\v3.5;%windir%\Microsoft.NET\Framework\v3.0;%windir%\Microsoft.NET\Framework\v2.0.50727;%PATH%
+echo @SET PATH=%windir%\Microsoft.NET\Framework\v3.5;%windir%\Microsoft.NET\Framework\v3.0;%windir%\Microsoft.NET\Framework\v2.0.50727;Microsoft.NET\Framework\v4.0.30319;%PATH%
+@SET PATH=%windir%\Microsoft.NET\Framework\v4.0.30319;%windir%\Microsoft.NET\Framework\v3.5;%windir%\Microsoft.NET\Framework\v3.0;%windir%\Microsoft.NET\Framework\v2.0.50727;Microsoft.NET\Framework\v4.0.30319;%PATH%
 
 ECHO Seasar.NETのビルドを開始します。
 REM TODO : エラー処理
