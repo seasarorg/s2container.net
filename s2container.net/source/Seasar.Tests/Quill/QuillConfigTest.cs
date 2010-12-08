141b36781900c1f22bcc275495bfd4d4ea2a2822
koyak <keikotani926@gmail.com>
2010-12-08 20:07:34 +0000

    不要なDLLを削除、現在は使用していないなテストをIgnore化
    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk@1240 6374060a-8204-0410-be21-af3d0958e6ef

--- s2container.net/source/Seasar.Tests/Quill/QuillConfigTest.cs
+++ s2container.net/source/Seasar.Tests/Quill/QuillConfigTest.cs
@@ -45,26 +45,31 @@ namespace Seasar.Tests.Quill
         private const string CONFIG_EMPTY = "QuillConfigTest_Empty";
         private const string CONFIG_FULL = "QuillConfigTest_Full";
 
+        /// <summary>
+        /// ConnectionStringを変更するだけで接続先を切り替えることができるか確認するためのテスト
+        /// </summary>
+        [Ignore("環境依存、実験用のテストのため")]
         [Test]
         public void TestChangeConnection()
         {
-            using (IDbConnection connection = new SqlConnection("Server=localhost;database=s2dotnetdemo;Integrated Security=SSPI"))
+            // 接続に失敗する接続文字列で初期化
+            using (IDbConnection connection = new SqlConnection("Server=xxxx"))
             {
                 try
                 {
                     connection.Open();
                     Assert.Fail();
                 }
-                catch (System.Exception)
+                catch (SqlException ex)
                 {
-                    Console.WriteLine("Exception occured.");
+                    Console.WriteLine("接続失敗 Exception={0},Message={1}", ex.GetType().Name, ex.Message);
                 }
 
-
+                // 接続に成功する文字列を指定
                 connection.ConnectionString = "Server=localhost\\SQLEXPRESS;database=s2dotnetdemo;Integrated Security=SSPI";
+                // 接続に成功する（ConnectionStringの変更のみで接続先を変更できる
                 connection.Open();
                 connection.Close();
-
             }
         }
 
