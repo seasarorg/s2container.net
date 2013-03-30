c76435767dda21da3a22b34ded91e42084a5e344
koyak <keikotani926@gmail.com>
2013-03-30 12:31:48 +0000

    [CONTAINERNET-157]ソースファイルとhtmlファイルのCopyrightを2013年版に更新する
    git-svn-id: https://www.seasar.org/svn/s2container.net/trunk@1362 6374060a-8204-0410-be21-af3d0958e6ef

---
+++ s2container.net/source/Seasar.Quill.Examples/Logic/IEmployeeLogic.cs
@@ -0,0 +1,11 @@
+﻿using Seasar.Quill.Attrs;
+using Seasar.Quill.Examples.Entity;
+
+namespace Seasar.Quill.Examples.Logic
+{
+    [Implementation(typeof(EmployeeLogic))]
+    public interface IEmployeeLogic
+    {
+        Employee GetEmployeeByEmpNo(int empNo);
+    }
+}
