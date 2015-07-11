3fe76603984f9dfc62b0f6bdf013c24bfc48b0c2
fujiihiroaki <jiifureit@jiifureit.ac>
2015-07-11 20:18:52 +0900

    ソースファイルとhtmlファイルのCopyright の"2005-2013"の文字列を"2005-2015"の文字列に更新する。

--- s2container.net/source/Seasar.DynamicProxy/Seasar.Framework.Aop/Impl/DynamicAopProxyAspectWeaver.cs
+++ s2container.net/source/Seasar.DynamicProxy/Seasar.Framework.Aop/Impl/DynamicAopProxyAspectWeaver.cs
@@ -1,6 +1,6 @@
 #region Copyright
 /*
-* Copyright 2005-2013 the Seasar Foundation and the Others.
+* Copyright 2005-2015 the Seasar Foundation and the Others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- s2container.net/source/Seasar.Quill/Properties/AssemblyInfo.cs
+++ s2container.net/source/Seasar.Quill/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 #region Copyright
 /*
- * Copyright 2005-2013 the Seasar Foundation and the Others.
+ * Copyright 2005-2015 the Seasar Foundation and the Others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Seasar.Quill")]
-[assembly: AssemblyCopyright("c Copyright The Seasar Foundation and the others 2005-2013, all rights reserved.")]
+[assembly: AssemblyCopyright("c Copyright The Seasar Foundation and the others 2005-2015, all rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
